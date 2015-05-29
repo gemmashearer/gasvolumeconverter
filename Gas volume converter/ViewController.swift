18dd60cb282a45df07a20b78933048390b3a2ec4
Gemma Shearer <gemmaandjude@gmail.com>
2015-05-29 15:14:39 +0100

    made the app

--- Gas volume converter/ViewController.swift
+++ Gas volume converter/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var molesOfGas: UITextField!
+    @IBOutlet weak var volumeOfGas: UILabel!
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+// the following code is disgusting and needs to be refactored
+    @IBAction func convertButton(sender: UIButton) {
+        let molesEntered = molesOfGas.text
+        var molesEnteredDouble = Double((molesEntered as NSString).doubleValue)
+        // as of now the figure shoould be a double - it shouldn't need unwrapping or anything
+        let molarVolumeOfGas = 24
+        volumeOfGas.hidden = false
+        molesEnteredDouble *= 24
+        let convertedGasVolume = "The volume is " + "\(molesEnteredDouble)" + " dm3"
+        volumeOfGas.text = convertedGasVolume
+        
+        }
 
+    //TODO convert the string to a double rather than an integer
 }
 
+
+
+
+
+        
